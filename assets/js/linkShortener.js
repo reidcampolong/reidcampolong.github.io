9f79391e2882fb42b4f85dbc78258a55713e6eb4
Reid Campolong <reidcampolong@gmail.com>
2019-05-29 01:58:52 -0400

    Updated link shortener URL

--- assets/js/linkShortener.js
+++ assets/js/linkShortener.js
@@ -4,6 +4,6 @@ checkForRedirect();
 
 function checkForRedirect() {
 	if(url.includes("slides")) {
-		window.location = "https://docs.google.com/presentation/d/1R1IX_9Xib0GI7dsJs4rZMycfQG39TKwKfHg4Ublt4Rc/edit?usp=sharing";
+		window.location = "https://docs.google.com/presentation/d/1bap2r0Yn9jL71NKWbyGwVFGONgKNzsl-5G6c5LB9KOQ/edit?usp=sharing";
 	}
 }
